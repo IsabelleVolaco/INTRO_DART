71b6991692628fa8c54d0cb89543a5ef5a7e3620
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-10-07 16:10:56 -0300

    exemplo ex 1 a 4

---
+++ Aula_07.10.dart
@@ -0,0 +1,85 @@
+void main() {
+  // Chamar cada exercício
+  /*
+  conversaoTemperatura();
+  contagemDe1a10();
+  verificacaoParOuImpar();
+  verificacaoIdadeParaVoto();
+  programaDeCreditoPessoal();
+  programaDeBrinde();
+  programaDeMilhagem();*/
+}
+
+
+// 1. Sistema de conversão de temperatura:
+/* Vamos converter Celsius para Fahrenheit.
+ Fórmula: °F = (°C x 1,8) + 32.
+ Temos duas variáveis "F" e "C". Realize a operação onde C = 22.
+ Qual o valor de F?*/
+
+void conversaoTemperatura() {
+
+  double celsius = 22;
+  double fahrenheit = (celsius * 1.8) + 32;
+  print("Temperatura em Fahrenheit: $fahrenheit");
+}
+
+
+// 2. Escreva um programa que conte de 1 até 10 usando a estrutura "do-while".
+
+void contagemDe1a10() {
+
+  int count = 1;
+  do {
+    print(count);
+    count++;
+  } while (count <= 10);
+}
+
+
+// 3. Escreva um programa que leia um número inteiro e verifique se ele é par ou ímpar.
+
+void verificacaoParOuImpar() {
+
+  int num = 7; 
+  if (num % 2 == 0) {
+    print("$num é par.");
+  } else {
+    print("$num é ímpar.");
+  }
+}
+
+
+// 4. Escreva um programa que verifique a idade sob as regras:
+ /*Menor de 16 não vota, de 16 até menor de 18 optativo,
+ 18 voto obrigatório e com 70 o voto é facultativo.*/
+
+void verificacaoIdadeParaVoto() {
+  
+  int idade = 20; 
+  if (idade < 16) {
+    print("Não vota.");
+  } else if (idade >= 16 && idade < 18 || idade >= 70) {
+    print("Voto optativo.");
+  } else {
+    print("Voto obrigatório.");
+  }
+}
+
+
+// 5. Escreva um programa de crédito pessoal, 
+/* que conceda até 30% do seu salário para a parcela e com juros de 12% no valor total.
+ Salário = 2.850,00.*/
+
+
+
+
+// 6. Escreva um programa de brinde onde temos de informar a idade e o sexo, 
+/* se for maior de idade ou se for do sexo feminino o participante ganha o brinde.*/
+
+
+
+
+// 7. Escreva um programa de milhagem onde a milhagem acumulada gera pontos,
+/* podendo trocar 12 milhas por 01 ponto e quando acumular 10 pontos 
+ o programa informa "contemplado".*/
