566db092a8001401b2d432c9bca9a7d3a9430068
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-10-07 14:48:44 -0300

    calculadora moda

---
+++ exercicios.dart
@@ -0,0 +1,30 @@
+int calcularModa(List<int> numeros) {
+  int moda = numeros[0];
+  int maxContagem = 0;
+
+  for (int i = 0; i < numeros.length; i++) {
+    int contagem = 0;
+    
+    // Conta quantas vezes o número aparece na lista
+    for (int j = 0; j < numeros.length; j++) {
+      if (numeros[j] == numeros[i]) {
+        contagem++;
+      }
+    }
+
+    // Atualiza a moda se encontrar um número com maior contagem
+    if (contagem > maxContagem) {
+      maxContagem = contagem;
+      moda = numeros[i];
+    }
+  }
+
+  return moda;
+}
+
+void main() {
+  List<int> numeros = [1, 3, 7, 3, 3, 2, 1, 7, 8, 3, 1];
+  int moda = calcularModa(numeros);
+
+  print("A moda é: $moda");
+}
