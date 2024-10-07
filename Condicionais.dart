2e726d7e1564a7f910ba7a60e5e476f3f0932c12
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-10-07 15:25:38 -0300

    exercicio switch e elseif

---
+++ Condicionais.dart
@@ -0,0 +1,43 @@
+
+ 
+ int nota = 7;
+ 
+ // EXEMPLO IF ELSE ELSEIF
+
+  if (nota == 10) {
+    print('Excelente');
+  } else if (nota >= 7) {
+    print('Bom');
+  } else if (nota >= 5) {
+    print('Suficiente');
+  } else {
+    print('Insuficiente');
+  }
+
+  // Saída: Bom
+
+
+// EXEMPLO SWITCH CASE
+
+ void verificarNota(int nota) {
+  switch (nota) {
+    case 10:
+      print('Excelente');
+      break;
+    case 7:
+    case 8:
+    case 9:
+      print('Bom');
+      break;
+    case 5:
+    case 6:
+      print('Suficiente');
+      break;
+    default:
+      print('Insuficiente');
+  }
+}
+
+verificarNota(nota);  // Saída: Bom
+
+ 
