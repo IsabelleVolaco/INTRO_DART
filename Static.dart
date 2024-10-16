40355dbf518139c7ced4119d27f47987c2fb81e9
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-10-16 16:44:39 -0300

    explicação

--- Static.dart
+++ Static.dart
@@ -11,3 +11,5 @@ void main() {
   Info.meta();
   print(Info.oi);
 }
+
+//static muda todos, não permite que nenhuma instância seja diferente, não permite que esta seja intanciada novamente. Ao alterar uma, altera todas. Por exemplo, ao alterar a cor de um Cão, todos os Animal, incluindo Passaro etc serão alteradas também.
