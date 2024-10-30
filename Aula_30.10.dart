940f1be548be2227dfa40d8d2a5f5ec69e4c977a
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-10-30 16:13:33 -0300

    ADD

--- Aula_30.10.dart
+++ Aula_30.10.dart
@@ -1,116 +1,119 @@
 void main() {
 //EXEMPLO 1
-  var lista = ["Laranja","Banana","Limão"];
-  print(lista);
+    var lista = ["Laranja","Banana","Limão"];
+    print(lista);
   
 
-  List lista2 = ["Laranja","Banana","Limão"];
-  print(lista2);
+    List lista2 = ["Laranja","Banana","Limão"];
+    print(lista2);
 
 
 //EXEMPLO 2
-  //List <String> lista = ["Laranja","Banana","Limão"];
-  List <int> numeros = [1,2,5];
-  List resultados = [2,"Vitor",22.4, 2, true];
-  //var resultados2 = [2,"Vitor",22.4, 2, true];
-  print(resultados);
+    //List <String> lista = ["Laranja","Banana","Limão"];
+    List <int> numeros = [1,2,5];
+    List resultados = [2,"Vitor",22.4, 2, true];
+    //var resultados2 = [2,"Vitor",22.4, 2, true];
+    print(resultados);
 
 
 //EXEMPLO 3
-  List <String> lista3 = ["Laranja","Banana","Limão"];
-  lista.add("Tomate");
-  print(lista3);
+    List <String> lista3 = ["Laranja","Banana","Limão"];
+    lista.add("Tomate");
+    print(lista3);
   
 
-  List <String> lista4 = ["Laranja","Banana","Limão"];
-  lista2.insert(1,"Tomate");
-  print(lista4);
+    List <String> lista4 = ["Laranja","Banana","Limão"];
+    lista2.insert(1,"Tomate");
+    print(lista4);
 
 
 //EXEMPLO 4
-  List <String> lista5 = ["Laranja","Banana","Limão"];
-  lista5.add("Tomate");
-  lista5.removeLast();
-  lista5.removeAt( 0 );
-  lista5.remove("Banana"); //se escrever errado ele nao exclui
-  print(lista5);
+    List <String> lista5 = ["Laranja","Banana","Limão"];
+    lista5.add("Tomate");
+    lista5.removeLast();
+    lista5.removeAt( 0 );
+    lista5.remove("Banana"); //se escrever errado ele nao exclui
+    print(lista5);
 
 
 //EXEMPLO 5
-  List <String> lista6 = ["Laranja","Banana","Limão"];
-  lista6.removeWhere((lista6) => lista6.length < 6);
-  print(lista6);
+    List <String> lista6 = ["Laranja","Banana","Limão"];
+    lista6.removeWhere((lista6) => lista6.length < 6);
+    print(lista6);
 
 
-  List <int> numbers = [5,8,2,4,1];
-  numbers.removeWhere((number) => number % 2 == 0); //remove os pares dentro do próprio escopo
-  print(numbers); 
+    List <int> numbers = [5,8,2,4,1];
+    numbers.removeWhere((number) => number % 2 == 0); //remove os pares dentro do próprio escopo
+    print(numbers); 
   
 
-  List <String> lista7 = ["Laranja","Banana","Limão"];
-  lista7.clear();
-  print(lista7);
+    List <String> lista7 = ["Laranja","Banana","Limão"];
+    lista7.clear();
+    print(lista7);
 
 
 //EXEMPLO 6
-  List <String> lista8 = ["Laranja","Banana","Limão"];
-  print(lista8.length); //3 itens na lista
+    List <String> lista8 = ["Laranja","Banana","Limão"];
+    print(lista8.length); //3 itens na lista
   
 
-  List <String> lista9 = ["Laranja","Banana","Limão"];
-  print(lista9.contains("Banana")); //true pois tem banana, fazendo uma validação
+    List <String> lista9 = ["Laranja","Banana","Limão"];
+    print(lista9.contains("Banana")); //true pois tem banana, fazendo uma validação
 
 
-  List <String> lista10 = ["Laranja","Banana","Limão"];
-  lista10[0]="Tomate"; //substitui o primeiro item
-  print(lista10);
+    List <String> lista10 = ["Laranja","Banana","Limão"];
+    lista10[0]="Tomate"; //substitui o primeiro item
+    print(lista10);
   
 
 //EXEMPLO 7
-  List<String> lista11 = ["Maçã", "Banana", "Laranja"];
-  for (var item in lista11) {
-    //'item' é um objeto sendo criado
-    print(item);
-  }
+    List<String> lista11 = ["Maçã", "Banana", "Laranja"];
+    for (var item in lista11) {
+        //'item' é um objeto sendo criado
+        print(item);
+    }
 
-  print(".............");
+    print(".............");
 
-  List<String> lista12 = ["Maçã", "Banana", "Laranja"];
-  for (var item in lista12) {
-    print(item.length);
-  }
+    List<String> lista12 = ["Maçã", "Banana", "Laranja"];
+    for (var item in lista12) {
+        print(item.length);
+    }
 
-  print(".............");
+    print(".............");
 
-  List<String> lista13 = ["Maçã", "Banana", "Laranja"];
-  for (var item in lista13) {
-    print(item.contains("Banana"));
-  }
+    List<String> lista13 = ["Maçã", "Banana", "Laranja"];
+    for (var item in lista13) {
+        print(item.contains("Banana"));
+    }
 
-  print(".............");
+    print(".............");
   
-  List<String> lista14 = ["Maçã", "Banana", "Laranja"];
-  lista14.forEach((item) => print(item));
+    List<String> lista14 = ["Maçã", "Banana", "Laranja"];
+    lista14.forEach((item) => print(item));
 
 
 //EXEMPLO 8
 
-  List<String> lista15 = ["Maçã", "Banana", "Laranja"];
-  print(lista15.join(', ')); //cria uma string que é uma junção de todos os elementos da lista, no caso, este também os separa por espaço e virgula.
-  print(lista15);
+    List<String> lista15 = ["Maçã", "Banana", "Laranja"];
+    print(lista15.join(', ')); //cria uma string que é uma junção de todos os elementos da lista, no caso, este também os separa por espaço e virgula.
+    print(lista15);
   
 
-  List<String> letras = ["A", "B", "C"];
-  String resulta = letras.join();
-  print(resulta);
+    List<String> letras = ["A", "B", "C"];
+    String resulta = letras.join();
+    print(resulta);
     
 
-  List<double> letras2 = [1,2,3];
-  String resulta2 = letras2.join(); //apenas junta
-  print(resulta2);
+    List<double> letras2 = [1,2,3];
+    String resulta2 = letras2.join(); //apenas junta
+    print(resulta2);
  
 
 //EXEMPLO 9
+
+    var variosTipos = [1, "texto", true, 3.14];
+
 
     List<dynamic> lista16 = [1, "texto", true, 3.14];
     print(lista16);
@@ -145,20 +148,31 @@ void main() {
 
 //EXEMPLO 11
 
-  //VETOR DE TAMANHO FIXO (5), COM ELEMENTOS PREENCHIDOS POR (0)
-  List<int> vetor = List.filled(5,0); 
-  vetor[0] = 10;
-  print(vetor);
-  /*vetor.add(6) dá erro pois não pode mudar o tamanho da lista*/
+    //VETOR DE TAMANHO FIXO (5), COM ELEMENTOS PREENCHIDOS POR (0)
+    List<int> vetor = List.filled(5,0); 
+    vetor[0] = 10;
+    print(vetor);
+    /*vetor.add(6) dá erro pois não pode mudar o tamanho da lista*/
 
-  //lista sem tamanho fixo, chamada de ARRAY DINAMICO
-  List<int> arrayDinamico = []; 
-  arrayDinamico.add(1);
-  arrayDinamico.add(2);
-  print(arrayDinamico);
+
+    //lista sem tamanho fixo, chamada de ARRAY DINAMICO
+    List<int> arrayDinamico = []; 
+    arrayDinamico.add(1);
+    arrayDinamico.add(2);
+    print(arrayDinamico);
     
 
+//EXEMPLO 12
+  var frutas = ['Banana','Maçã','Laranja'];
+  frutas.add('Abacaxi');
+  print(frutas);
+  
+  
+  var frutas2= ['Banana','Maçã','Laranja'];
+  frutas2.addAll(['Abacaxi','Morango','Uva']);
+  print(frutas2);
 
-
-
+  
 }
+
+
