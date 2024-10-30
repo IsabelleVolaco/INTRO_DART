a201cd6e62df615b578c12846ffd6b30b34c6169
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-10-30 14:39:57 -0300

    LENGTH, CONTAINS E SUBSTITUIÇÃO

--- Aula_30.10.dart
+++ Aula_30.10.dart
@@ -6,6 +6,7 @@ void main() {
   List lista2 = ["Laranja","Banana","Limão"];
   print(lista2);
 
+
 //EXEMPLO 2
   //List <String> lista = ["Laranja","Banana","Limão"];
   List <int> numeros = [1,2,5];
@@ -23,6 +24,7 @@ void main() {
   lista2.insert(1,"Tomate");
   print(lista4);
 
+
 //EXEMPLO 4
   List <String> lista5 = ["Laranja","Banana","Limão"];
   lista5.add("Tomate");
@@ -30,7 +32,7 @@ void main() {
   lista5.removeAt( 0 );
   lista5.remove("Banana"); //se escrever errado ele nao exclui
   print(lista5);
-}
+
 
 //EXEMPLO 5
   List <String> lista6 = ["Laranja","Banana","Limão"];
@@ -38,9 +40,26 @@ void main() {
   print(lista6);
 
   List <int> numbers = [5,8,2,4,1];
-  numbers.removeWhere((number) => number % 2 == 0); //remove os pares
+  numbers.removeWhere((number) => number % 2 == 0); //remove os pares dentro do próprio escopo
   print(numbers); 
   
   List <String> lista7 = ["Laranja","Banana","Limão"];
   lista7.clear();
   print(lista7);
+
+
+//EXEMPLO 6
+  List <String> lista8 = ["Laranja","Banana","Limão"];
+  print(lista8.length); //3 itens na lista
+  
+  List <String> lista9 = ["Laranja","Banana","Limão"];
+  print(lista9.contains("Banana")); //true pois tem banana
+
+  List <String> lista10 = ["Laranja","Banana","Limão"];
+  lista10[0]="Tomate"; //substitui o primeiro item
+  print(lista10);
+  
+
+
+
+}
