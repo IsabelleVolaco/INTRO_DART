6350510a244f501e213aedeb9c336d2c248a82c5
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-10-30 13:50:05 -0300

    listas com tipos

--- Aula_30.10.dart
+++ Aula_30.10.dart
@@ -1,7 +1,15 @@
 void main() {
+//EXEMPLO 1
   var lista = ["Laranja","Banana","Limão"];
   print(lista);
   
   List minhaLista = ["Laranja","Banana","Limão"];
   print(minhaLista);
+
+//EXEMPLO 2
+  List <String> lista = ["Laranja","Banana","Limão"];
+  List <int> numeros = [1,2,5];
+  List resultados = [2,"Vitor",22.4, 2, true];
+  var resultados2 = [2,"Vitor",22.4, 2, true];
+  print(resultados);
 }
